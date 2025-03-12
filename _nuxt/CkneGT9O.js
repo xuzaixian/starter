da799307da21894e8d8cbd100c28b102d57af11e
xuzaixian <xuzaixian1229@gmail.com>
2025-03-12 07:06:57 +0000

    Deploying to gh-pages from @ xuzaixian/starter@24462b031a3c6ad04b8af90d7ac63002acce0fc2 🚀

---
+++ _nuxt/CkneGT9O.js
@@ -0,0 +1,1 @@
+import{a as o,f as r,k as a,R as s,ae as l,l as n}from"./Bbdj-k7Y.js";const c=o({__name:"Alert",props:{color:{type:String,default:"orange"}},setup(e){return(t,d)=>(a(),r("div",{class:"alert",style:l({"border-color":e.color})},[s(t.$slots,"default",{},void 0,!0)],4))}}),p=Object.assign(n(c,[["__scopeId","data-v-46dbbe98"]]),{__name:"Alert"});export{p as default};
---
+++ _nuxt/D9rXrRAl.js
@@ -0,0 +1,1 @@
+import{l as o,f as t,k as s,R as c}from"./Bbdj-k7Y.js";const r={};function n(e,l){return s(),t("blockquote",null,[c(e.$slots,"default")])}const _=Object.assign(o(r,[["render",n]]),{__name:"ProseBlockquote"});export{_ as default};
